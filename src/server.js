0576ea6312dd58bce148353ba808f4f913208e00
catalunha <catalunha.mj@gmail.com>
2022-05-04 18:19:53 -0300

    toekn

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ app.post('/test', async (req,res)=>{
       pixReceived.set('valor',pix.valor);
       pixReceived.set('horario',new Date(pix.horario));
       pixReceived.set('infoPagador',pix.infoPagador);
-      const result = await pixReceived.save();
+      const result = await pixReceived.save({sessionToken:user.sessionToken});
       console.log('PixReceived: id created',result.id);
     } catch (error) {
       console.log('PixReceived: error');
